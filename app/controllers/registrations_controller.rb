bbcf10992380fbcc9363902f42cdc51809fca2bc
Ivan <ivan@ip-larionov.ru>
2015-03-05 10:10:03 +0700

    user profile edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,4 +20,8 @@ class RegistrationsController < Devise::RegistrationsController
         :email, :password, :password_confirmation, :current_password)
     end
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
