712773e267ef2bb087b8c03775699ed150ebfb96
Ivan <ivan@ip-larionov.ru>
2015-03-11 01:45:21 +0700

    basic adding of credit card

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -4,7 +4,7 @@ class CreditCard < ActiveRecord::Base
   attr_accessor :number, :exp_month, :exp_year, :cvc, :name
   validates :user_id, :presence => true
 
-  after_commit :create_stripe_card, :on => [:create]
+  before_create :create_stripe_card
 
   def create_stripe_card
     token = Stripe::Token.create(
@@ -16,7 +16,8 @@ class CreditCard < ActiveRecord::Base
         :cvc => self.cvc
       }
     )
-    puts token.inspect
-    self.update_attributes :stripe_id => token['card']['id'], :last4 => self.number.split(//).last(4).join
+    # puts token.inspect
+    self.stripe_id = token['card']['id']
+    self.last4 = self.number.split(//).last(4).join
   end
 end
