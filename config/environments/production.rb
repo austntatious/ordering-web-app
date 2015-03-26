8af3a0d7150a6c45ef12813844f8c6f99a293691
Ivan <ivan@ip-larionov.ru>
2015-03-26 10:31:20 +0700

    sendmail + footer links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,11 +75,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :port => ENV['SMTP_PORT'],
-    :address => ENV['SMTP_SERVER'],
-    :user_name => ENV['SMTP_USER'],
-    :password => ENV['SMTP_PASSWORD']
-  }
+  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.smtp_settings = {
+  #   :port => ENV['SMTP_PORT'],
+  #   :address => ENV['SMTP_SERVER'],
+  #   :user_name => ENV['SMTP_USER'],
+  #   :password => ENV['SMTP_PASSWORD']
+  # }
 end
