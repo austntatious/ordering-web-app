6236f9c66ba9c62297a420d1b1fc52b3342876fd
Ivan <ivan@ip-larionov.ru>
2015-05-05 07:54:05 +0700

    404

--- app/controllers/text_pages_controller.rb
+++ app/controllers/text_pages_controller.rb
@@ -3,6 +3,9 @@ class TextPagesController < InheritedResources::Base
 
   def show
     @text_page = TextPage.find_by_url(params[:url])
+    if @text_page.nil?
+      TextPage.find(-1)
+    end
   end
 end
 
